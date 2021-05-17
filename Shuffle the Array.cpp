44030ba3bbdfd65c0f8a89d5b2ed7f02c9663310
IsraaAbdElnaby <israa.tobgy@gmail.com>
2021-05-17 17:03:55 +0200

    Solution Modified

---
+++ Shuffle the Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> output;
+        int i = 0, j = n;
+        while (i < n) {
+            output.push_back(nums[i]);
+            output.push_back(nums[j]);
+            i++; 
+            j++;
+        }
+        return output;
+    }
+};
